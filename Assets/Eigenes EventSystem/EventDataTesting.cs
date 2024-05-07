a010f9ac002d7915c7b48d35f4da04b65dcc2133
Kim Bolender <kimbolender99@gmail.com>
2024-05-07 21:51:49 +0200

    insert of event manager test system

---
+++ Assets/Eigenes EventSystem/EventDataTesting.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+public class EventDataTesting
+{
+    /// <summary>
+    /// Type of the event. Every event must declare an event type.
+    /// </summary>
+    public EventDataTypeTesting Type { get; private set; }
+
+    /// <summary>
+    /// Data stored within the event. Some events may not include any kind of data.
+    /// </summary>
+    public object Data { get; private set; }
+
+    /// <summary>
+    /// The source of the event. Used for tracking purpose during debugging.
+    /// </summary>
+    public string Source { get; private set; }
+
+    public EventDataTesting(EventDataTypeTesting type) : this(type, null)
+    {
+    }
+
+    public EventDataTesting(EventDataTypeTesting type, object data)
+    {
+        Type = type;
+        Data = data;
+        Source = null;
+
+#if UNITY_EDITOR
+        StackFrame sourceFrame = new StackFrame(2);
+        Source = sourceFrame.GetMethod().ReflectedType.Name + "." + sourceFrame.GetMethod().Name;
+#endif
+    }
+}
---
+++ Assets/Eigenes EventSystem/EventManagerTesting.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Basic implementation of the event manager.
+/// Simple and straight forward without fancy optimizations.
+/// </summary>
+public class EventManagerTesting : MonoBehaviour
+{
+    public static EventManagerTesting MainStatic;
+    /// <summary>
+    /// Currently registered listeners.
+    /// Any incoming event will be delegated to these listeners.
+    /// </summary>
+    List<EventListenerTesting> listeners;
+
+    /// <summary>
+    /// Collection of events fired during the last frame.
+    /// </summary>
+    List<EventDataTesting> events;
+
+    private void Awake()
+    {
+        MainStatic = this;
+    }
+
+    /// <summary>
+    /// Usually replaced by a script that initializes all standard systems
+    /// </summary>
+    private void Start()
+    {
+        Initialize();
+    }
+
+    /// <summary>
+    /// Event manager setup.
+    /// </summary>
+    public void Initialize()
+    {
+        listeners = new List<EventListenerTesting>();
+        events = new List<EventDataTesting>();
+    }
+
+    /// <summary>
+    /// Add a listener that will receive events.
+    /// </summary>
+    /// <param name="listener">The event listener to be added.</param>
+    public void Update()
+    {
+        // Make sure no new events are added to the list that is now executed.
+        List<EventDataTesting> eventsToFireNow = events;
+        events = new List<EventDataTesting>();
+
+        foreach (EventDataTesting eventToFire in eventsToFireNow)
+        {
+            foreach (EventListenerTesting listener in listeners)
+            {
+                listener.OnEventReceived(eventToFire);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Add a listener that will receive events.
+    /// </summary>
+    /// <param name="listener">The event listener to be added.</param>
+    public void AddListener(EventListenerTesting listener)
+    {
+        if (listener != null && !listeners.Contains(listener))
+        {
+            listeners.Add(listener);
+        }
+    }
+
+    /// <summary>
+    /// Remove an existing listener so that it will not receive events anymore.
+    /// </summary>
+    /// <param name="listener">The event listener to be removed.</param>
+    public void RemoveListener(EventListenerTesting listener)
+    {
+        listeners.Remove(listener);
+    }
+
+    /// <summary>
+    /// Queue an event for execution during the next update cycle.
+    /// </summary>
+    /// <param name="eventToFire">Event to be delegated to its listeners.</param>
+    public void FireEvent(EventDataTesting eventToFire)
+    {
+        events.Add(eventToFire);
+    }
+}
